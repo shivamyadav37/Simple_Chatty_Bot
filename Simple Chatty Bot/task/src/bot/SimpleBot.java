4c848b021ce275e8bc384950cd2abbf143afe894
shivamyadav37 <shivam.yadav_cs18@gla.ac.in>
2020-02-11 22:57:20 +0530

    Commenting in Java

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -1,24 +1,9 @@
 package bot;
 
-import java.util.Scanner;
-
 public class SimpleBot {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+        // write your code here
         System.out.println("Hello! My name is Veronica.");
         System.out.println("I was created in 2020.");
-        System.out.println("Please, remind me your name.");
-
-        // reading a name
-        Scanner s = new Scanner(System.in);
-        String Username = s.nextLine();
-        System.out.println("What a great name you have," +Username +"!");
-        System.out.println("Let me guess your age.");
-        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
-
-        // reading all remainders
-
-        System.out.println("Your age is {yourAge}; that's a good time to start programming!");
     }
 }
--- Simple Chatty Bot/task/test/PersonalAssistantTest.java
+++ Simple Chatty Bot/task/test/PersonalAssistantTest.java
@@ -1,79 +1,45 @@
 import bot.SimpleBot;
-import org.hyperskill.hstest.v6.stage.BaseStageTest;
 import org.hyperskill.hstest.v6.testcase.CheckResult;
+import org.hyperskill.hstest.v6.stage.BaseStageTest;
 import org.hyperskill.hstest.v6.testcase.TestCase;
 
 import java.util.List;
 
 
-class Clue {
-    int age;
-    String name;
-
-    Clue(String name, int age) {
-        this.age = age;
-        this.name = name;
-    }
-}
-
-
-public class PersonalAssistantTest extends BaseStageTest<Clue> {
+public class PersonalAssistantTest extends BaseStageTest {
 
     public PersonalAssistantTest() {
         super(SimpleBot.class);
     }
 
     @Override
-    public List<TestCase<Clue>> generate() {
+    public List<TestCase> generate() {
         return List.of(
-            new TestCase<Clue>()
-                .setInput("John\n1 2 1")
-                .setAttach(new Clue("John", 22)),
-
-            new TestCase<Clue>()
-                .setInput("Nick\n2 0 0")
-                .setAttach(new Clue("Nick", 35))
+            new TestCase<>()
         );
     }
 
     @Override
-    public CheckResult check(String reply, Clue clue) {
+    public CheckResult check(String reply, Object clue) {
 
         String[] lines = reply.trim().split("\n");
 
-        if (lines.length != 7) {
+        if (lines.length != 2) {
             return CheckResult.FALSE(
-                "You should output 7 lines. Lines found: " + lines.length + "\n" +
-                "Your output:\n" +
-                reply
+                "You should output exactly 2 lines!\n" +
+                "Lines found: " + lines.length
             );
         }
 
-        String lineWithName = lines[3].toLowerCase();
-        String name = clue.name.toLowerCase();
+        String secondLine = lines[1];
 
-        if (!lineWithName.contains(name)) {
+        if (!secondLine.matches(".*\\d.*")) {
             return CheckResult.FALSE(
-                "The name was " + clue.name + "\n" +
-                "But the 4-th line was:\n" +
-                "\"" + lines[3] + "\"\n\n" +
-                "4-th line should contain a name of the user"
-            );
-        }
-
-        String lineWithAge = lines[6].toLowerCase();
-        String age = Integer.toString(clue.age);
-
-        if (!lineWithAge.contains(age)) {
-            return CheckResult.FALSE(
-                "Can't find a correct age " +
-                "in the last line of output! " +
-                "Maybe you calculated the age wrong?\n\n" +
-                "Your last line: \n" + "\"" + lines[6] + "\""
+                "The second line should contain a year!\n" +
+                "Your second line: \"" + secondLine + "\""
             );
         }
 
         return CheckResult.TRUE;
     }
-
 }
