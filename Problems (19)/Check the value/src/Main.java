019f59d197f7e112fe245b3f2805c9d40d0b861e
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-02-17 00:26:14 +0530

    Problem

---
+++ Problems (19)/Check the value/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        if (a <10){
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
+
+    }
+}
