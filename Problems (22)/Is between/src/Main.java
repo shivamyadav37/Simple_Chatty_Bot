5ac9450ad0da59c96aca0add5cd7efd4627a3988
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-02-17 22:15:33 +0530

    Checking a Number In between two Numbers

---
+++ Problems (22)/Is between/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int firstNumber = scanner.nextInt();
+        int secondNumber = scanner.nextInt();
+        int thirdNumber = scanner.nextInt();
+        if (firstNumber>=secondNumber && firstNumber<=thirdNumber){
+            System.out.println("true");
+        }
+        else if (firstNumber<=secondNumber && firstNumber>=thirdNumber)
+        {
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
+    }
+}
