886870f7d79b406c019a95d01eff99323954533e
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-02-20 18:25:02 +0530

    Problem_29

--- Problems (29)/Maximum element divisible by 4/src/Main.java
+++ Problems (29)/Maximum element divisible by 4/src/Main.java
@@ -3,6 +3,15 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int numberOfElements = scanner.nextInt();
+        int elements = scanner.nextInt();
+        int i = 0;
+        int counter = 0;
+        for(i = 0 ; i <numberOfElements ; i++){
+            if(elements %4== 0)
+                counter++;
 
+        }
     }
+
 }
