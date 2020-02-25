7e912aeaac3d232a52b2e47f922169f7e1e31593
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-02-25 15:26:00 +0530

    Daily Problem

--- Problems (31)/The product of numbers from a to b/src/Main.java
+++ Problems (31)/The product of numbers from a to b/src/Main.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int counter= 0;
-        int res = 0;
-        for (int i =a ; i<b ; i++){
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+        long counter= 0;
+        long res = 0;
+        for (long i =a ; i<b ; i++){
             //counter++;
+            //b = i*b;
+            res++;
             res = res*b;
             //res = i;
         }
---
+++ Problems (32)/Check a number is positive/src/Main.java
@@ -0,0 +1,18 @@
+//Daily Problem
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        if(a>0){
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
+    }
+
+}
