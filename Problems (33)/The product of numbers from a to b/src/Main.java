47c5709c0306193e2036787e058b1469d9003588
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-02-29 09:11:32 +0530

    For Loop Problems__33_-The_product_of_numbers_from_a_to_b_test.iml

---
+++ Problems (33)/The product of numbers from a to b/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+    static int product(int arr[],int n) {
+        int result = 1;
+        for (int i = 0; i < n; i++)
+            result = result * arr[i];
+        return result;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int n= arr.length;
+
+        }
+
+    }
