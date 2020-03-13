ec6d872acea2570a93a83f578106dac521adce59
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-03-13 22:06:59 +0530

    Problems__40_-Collatz_conjecture.iml

--- Problems (40)/Collatz conjecture/src/Main.java
+++ Problems (40)/Collatz conjecture/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n%2==0){
+            System.out.println(n/2);
+        }
+        else {
+            n=(n*3)+1
+            for (int i =)
+        }
     }
+
 }
