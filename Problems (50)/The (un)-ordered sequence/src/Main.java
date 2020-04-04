d89aa3a651d08ed8726d4a3b9003593a507f9873
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-04-04 20:59:55 +0530

    Problem on Ordered Sequence

--- Problems (50)/The (un)-ordered sequence/src/Main.java
+++ Problems (50)/The (un)-ordered sequence/src/Main.java
@@ -1,5 +1,47 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ArrayIndexOutOfBoundsException {
+        Scanner sc = new Scanner(System.in);
+        String[] myStrArray = sc.nextLine().split(" ");
+        boolean asc = false;
+        boolean desc = false;
+
+        for (String s : myStrArray) {
+            System.out.print(s + " ");
+        }
+
+        // Check ascending
+        for (int i = 0; i < myStrArray.length; i++) {
+            String[] arrayWithoutZero = Arrays.copyOf(myStrArray, myStrArray.length - 1);
+            if (Integer.parseInt(arrayWithoutZero[i]) >= Integer.parseInt(myStrArray[i + 1])) {
+                asc = true;
+            } else {
+                asc = false;
+                break;
+            }
+        }
+
+
+        // Check descending
+        for (int i = 0; i < myStrArray.length; i++) {
+            String[] arrayWithoutZero = Arrays.copyOf(myStrArray, myStrArray.length - 1);
+            if (Integer.parseInt(arrayWithoutZero[i]) <= Integer.parseInt(myStrArray[i + 1])) {
+                desc = true;
+            } else {
+                desc = false;
+                break;
+            }
+        }
+
+        if (myStrArray.length <= 1) {
+            System.out.println(true);
+        } else if (asc || desc) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
 
     }
 }
